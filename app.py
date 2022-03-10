bb227e24c740cdc018b2dd85954cd064b5c9eeb1
Stexe23 <98385650+Stexe23@users.noreply.github.com>
2022-03-10 18:15:18 +0500

    Добаление коментариев по тексту кода

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
-
 import telebot
-from extensions import APIException, ValueConvert
-from config import TOKEN, key
+from extensions import APIException, ValueConvert  # Импорт классов
+from config import TOKEN, key  # Импорт ключей бота и валют
 
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def set_welcome(message: telebot.types.Message):
+@bot.message_handler(commands='start')  # Обработчик начала работы
+def start_(message: telebot.types.Message):
+    text = 'Приветствую.\n' \
+           'Для продолжения введите /help'
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands='help')  # Обработчик помощи
+def help_(message: telebot.types.Message):
     text = 'Чтобы начать работу, введите команду в следующем формате:\n <имя валюты> ' \
            '<в какую валюту перевести> ' \
-           '<количество переводимой валюты>'
-    bot.reply_to(message, text)
+           '<количество переводимой валюты>\n' \
+           'Увидеть список доступных валют: /values'
+    bot.send_message(message.chat.id, text)
 
 
-@bot.message_handler(commands='values')
+@bot.message_handler(commands='values')  # Обработчик списка доступных валют
 def values(message: telebot.types.Message):
     text = 'Доступные валюты'
     for i in key.keys():
@@ -22,17 +29,21 @@ def values(message: telebot.types.Message):
     bot.send_message(message.chat.id, text)
 
 
-@bot.message_handler(content_types=['text', ])
+@bot.message_handler(content_types=['text'])  # Обработчик конвертора валют
 def conver(message: telebot.types.Message):
-    value = message.text.split(' ')
+    try:
+        value = message.text.split(' ')
 
-    if len(value) != 3:
-        raise APIException('Слишком много пораметров')
+        if len(value) != 3:  # Обязательно: три параметра ввода
+            raise APIException('Слишком мало/много пораметров')
 
-    quote, base, amount = value
-    resp = ValueConvert.convert(quote, base, amount)
-    text = f'Итого: {amount} {quote} равно {resp} {base}'
-    bot.send_message(message.chat.id, text)
+        resp = ValueConvert.get_price(*value)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        bot.send_message(message.chat.id, resp)
 
 
 bot.polling()
--- extensions.py
+++ extensions.py
@@ -3,32 +3,35 @@ import requests
 import json
 
 
-class APIException(Exception):
+class APIException(Exception):  # Класс ошибок
     pass
 
 
-class ValueConvert:
+class ValueConvert:  # Класс для конверитрования волюты
     @staticmethod
-    def convert(quote: str, base: str, amount: str):
-        if quote == base:
+    def get_price(quote: str, base: str, amount: str):  # Функция обработки конвертируемых волют
+        try:
+            quote_ticker = key[quote.lower()]  # Переводимая валюта. Понижаем регистр напсания
+        except KeyError:
+            raise APIException(f'Валюта {quote} не найдена!')
+
+        if quote == base:  # Если валюта одна и таже
             raise APIException(f'Невозможно перевести одинаковые валюты {quote}')
 
         try:
-            quote_ticker = key[quote]
+            base_ticker = key[base.lower()]  # Получаемая валюта. Понижаем регистр написания
         except KeyError:
-            raise APIException(f'Не удалось обработать валюту {quote}')
+            raise APIException(f'Валюта {base} не найдена!')
 
         try:
-            base_ticker = key[base]
-        except KeyError:
-            raise APIException(f'Не удалось обработать валюту {base}')
-
-        try:
-            amount = float(amount)
+            amount = float(amount)  # Число с плавоющей точкой
         except ValueError:
             raise APIException(f'Не удалось обработать количество {amount}')
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={key[quote]}&tsyms={key[base]}')
-        resp = json.loads(r.content)[key[base]]
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        resp = json.loads(r.content)
+        resp_sum = resp[base_ticker] * amount  # Вычислеение значений после конвертации валюты
+        resp_sum = round(resp_sum, 4)  # Округление до 4 знаков после запятой
+        message = f"Цена {amount} {quote} в {base} : {resp_sum}"
 
-        return resp
+        return message
